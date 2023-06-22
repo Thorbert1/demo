bfb3131e8fbf2cc2479375aa7ba86c3d97a293e0
Thorbert1 <70276772+Thorbert1@users.noreply.github.com>
2023-06-22 22:33:56 +0700

    Initial commit

---
+++ src/main/java/com/tobtoby/App.java
@@ -0,0 +1,13 @@
+package com.tobtoby;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
